d9d0dd6397915a1feb3012438b131025d24b876d
Apekshya Koirala <apekshya224@gmail.com>
2025-10-11 11:45:28 +0545

    signup setup completed except value storage management

--- script.js
+++ script.js
@@ -24,3 +24,59 @@ next.addEventListener("click", () => {
 });
 
 showSlide(currentIndex)
+
+
+
+
+const logsigns = document.querySelectorAll("#login, #signup")
+
+logsigns.forEach(button => {
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
+        // e.stopPropagation();
+        const maincontent = document.getElementsByClassName("cover")[0]
+        maincontent.classList.add("blur")
+        if (e.currentTarget.id === "login") {
+            show("login.html")
+        }
+        else {
+            show("signup.html")
+        }
+    })
+})
+
+function show(componentFile) {
+    const container = document.getElementById("modalContainer")
+    container.innerHTML = "";
+  fetch(componentFile)
+    .then((res) => res.text())
+      .then((data) => {
+        console.log("done")
+        container.innerHTML = data;
+          container.style.display = "block"
+
+          const btns = container.getElementsByClassName("submit");
+          Array.from(btns).forEach((btn) => {
+            btn.addEventListener("click", function (e) {
+              e.preventDefault();
+              const inputs = container.querySelectorAll(".userinput");
+              const values = [];
+                inputs.forEach((input) => values.push(input.value));
+                removeComponent();
+            //   console.log(values);
+            });
+          });
+
+      });
+    
+}
+
+function removeComponent() {
+    const container = document.getElementById("modalContainer");
+    container.style.display="none"
+    container.innerHTML = "";
+     const maincontent = document.getElementsByClassName("cover")[0];
+     maincontent.classList.remove("blur");
+}
+
+
